28e28a94a0115906803b3cb4462921c77ee73734
jinsilna <zxcv392766!>
2021-08-10 20:21:02 +0900

    메모 날짜 DateFormatter 사용하여 간결하게 수정

--- Memo/MemoListTableViewController.swift
+++ Memo/MemoListTableViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class MemoListTableViewController: UITableViewController {
 
+    let fomatter: DateFormatter = {
+        let f = DateFormatter()
+        f.dateStyle = .long
+        f.timeStyle = .none
+        f.locale = Locale(identifier: "Ko_kr")
+        return f
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -32,7 +40,7 @@ class MemoListTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         let target = Memo.dummyMemoList[indexPath.row]
         cell.textLabel?.text = target.content
-        cell.detailTextLabel?.text = "\(target.insertDate)"
+        cell.detailTextLabel?.text = fomatter.string(from: target.insertDate)
 
         return cell
     }
